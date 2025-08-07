b9fbdb9c45edf69450d8b72686d6177111329059
Roshan Patil <51580071+roshanpatil2000@users.noreply.github.com>
2025-08-07 07:46:41 +0000

    removed everything unnecessary

--- routes/contactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { contactHandler } = require("../controllers/contactController");
-
-router.post("/contact", contactHandler);
-
-module.exports = router;
